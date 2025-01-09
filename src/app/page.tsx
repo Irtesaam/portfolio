b6082f04cc80d452b62016ee23f15211a491186e
Irtesaam <atfimdirtesaam@gmail.com>
2025-01-10 02:07:33 +0530

    integrated speed insights by vercel to check performance metrics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,10 +10,12 @@ import { Gallery, Headings } from "@/components/gallery";
 import { Contact } from "@/components/contact";
 import Footer from "@/components/footer";
 import { FloatingDockDemo } from "@/components/mobile-dock";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Home() {
     return (
         <main className="relative dark:bg-black-100 bg-white flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+            <SpeedInsights />
             <ThemeToggle />
             <FloatingNav navItems={navItems} className="hidden md:flex" />
             <Hero />
