9496352cc52a90ef4e58b59c874a378d4d02b4ff
Irtesaam <atfimdirtesaam@gmail.com>
2025-01-15 01:21:48 +0530

    added 2 more projects

--- src/data/index.ts
+++ src/data/index.ts
@@ -6,23 +6,39 @@ export const navItems = [
     { name: "Experience", link: "#experience" },
     { name: "Gallery", link: "#gallery" },
     { name: "Contact", link: "#contact" },
-  ];
+];
 
-  export const projects = [
+export const projects = [
     {
-      id: 1,
-      title: "A Single Page Static Website",
-      des: "Website build using modern technologies like Next.js, Tailwind CSS, TypeScript, Framer Motion, React Email, Resend and Vercel hosting",
-      img: "/projects/p1.jpeg",
-      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/re.svg", "/fm.svg"],
-      link: "https://github.com/Irtesaam/nextjs_website",
+        id: 1,
+        title: "npx Card",
+        des: "The npx card offers multiple ways to connect with me by running npx irtesaam from any terminal worldwide",
+        img: "/projects/p3.png",
+        iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+        link: "https://github.com/Irtesaam/npxCard",
     },
     {
-      id: 2,
-      title: "Bootsrap Travel Blog",
-      des: "Made entirely with Bootstrap components without any custom CSS and has Responsive Design across all devices.",
-      img: "/projects/p2.jpeg",
-      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
-      link: "https://github.com/Irtesaam/travellog",
+        id: 2,
+        title: "Portfolio Website",
+        des: "Here I document my journey and insights that cannot be summed in a single-page resume",
+        img: "/projects/p4.png",
+        iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+        link: "https://github.com/Irtesaam/portfolio",
     },
-  ];
+    {
+        id: 3,
+        title: "A Single Page Static Website",
+        des: "Website build using modern technologies like Next.js, Tailwind CSS, TypeScript, Framer Motion, React Email, Resend and Vercel hosting",
+        img: "/projects/p1.jpeg",
+        iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/re.svg", "/fm.svg"],
+        link: "https://github.com/Irtesaam/nextjs_website",
+    },
+    {
+        id: 4,
+        title: "Bootsrap Travel Blog",
+        des: "Made entirely with Bootstrap components without any custom CSS and has Responsive Design across all devices.",
+        img: "/projects/p2.jpeg",
+        iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+        link: "https://github.com/Irtesaam/travellog",
+    },
+];
